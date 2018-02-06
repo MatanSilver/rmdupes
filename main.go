8517e2b87271985598b6e1f3068a8c06aa1fbf58
MatanSilver <matansilver@gmail.com>
2018-02-05 20:05:10 -0500

    added total size

--- main.go
+++ main.go
@@ -19,21 +19,32 @@ type FileInfoWrapper struct {
 
 // Takes in a directory path. Recursively crawls the directory and outputs a
 // list of paths of files in that directory and subdirectories
-func ls(dir string) []FileInfoWrapper {
+func ls(dir string) ([]FileInfoWrapper, int64) {
+	var totalsize int64 = 0
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		//log.Fatalf("dir: %s, err: %s", dir, err)
-		log.Println(err)
+		if len(files) == 0 {
+			return []FileInfoWrapper{}, 0
+		}
+		log.Fatalf("Error reading directory: %v\n\t%v", dir, err)
 	}
 	var fileinfos []FileInfoWrapper
 	for _, file := range files {
 		if file.IsDir() {
-		} else {
 			//if the file is a directory, recursively add the directory's contents
-			if file.Name() != ".git" { //kindly ignore .git folder to avoid spam
+			log.Printf("Recursing into %s\n", file.Name())
+			if file.Name() != ".git" && file.Name() != ".DS_Store" { //kindly ignore .git folder to avoid spam
 				//log.Printf("entering directory: %s\n", file.Name())
-				fileinfos = append(fileinfos, ls(strings.Join([]string{dir, file.Name()}, "/"))...)
+				newfileinfos, newtotalsize := ls(strings.Join([]string{dir, file.Name()}, "/"))
+				totalsize += newtotalsize
+				fileinfos = append(fileinfos, newfileinfos...)
 			}
+		} else {
+			if file.Size() > 256000000 {
+				log.Printf("Skipping large file %s\n", file.Name())
+				break
+			}
+			totalsize += file.Size()
 			fullpath := strings.Join([]string{dir, file.Name()}, "/")
 			//now we generate a hash, which might be useful for checking for
 			//duplicates
@@ -43,30 +54,28 @@ func ls(dir string) []FileInfoWrapper {
 			}
 			hasher := sha256.New()
 			hasher.Write(buff)
-			if err != nil {
-				log.Fatal(err)
-			}
+			//TODO possibly remove use of hex and just put to []byte
 			fileinfo := FileInfoWrapper{file, fullpath, hex.EncodeToString(hasher.Sum(nil))}
 			fileinfos = append(fileinfos, fileinfo)
 		}
 	}
-	return fileinfos
+	return fileinfos, totalsize
 }
 
 func RmDupes(dryrun bool) {
-	fileinfos := ls(".")
+	fileinfos, totalsize := ls(".")
 	fileinfos_map := map[string]string{}
 	for _, fileinfo := range fileinfos {
 		if _, ok := fileinfos_map[fileinfo.Hash]; !ok {
 			fileinfos_map[fileinfo.Hash] = fileinfo.Path
 		} else {
-			log.Println("File flagged for removal: %s", fileinfo.Path)
+			log.Printf("File flagged for removal: %s\n", fileinfo.Path)
 			if !dryrun {
 				os.Remove(fileinfo.Path)
 			}
 		}
 	}
-
+	log.Printf("Total space cleared: %d\n", totalsize)
 }
 
 func main() {
@@ -75,7 +84,7 @@ func main() {
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
 			Name:  "dry-run",
-			Usage: "only print logs of files to be deleted or errors",
+			Usage: "Only print logs of files to be deleted or errors",
 		},
 	}
 	app.Usage = "Removes duplicate files by _content_"
